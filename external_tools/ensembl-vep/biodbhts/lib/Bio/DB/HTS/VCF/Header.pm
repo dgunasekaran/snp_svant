cd74d641897aafac20ebd17187b48fc7d705c9b4
dgunasekaran <47905038+dgunasekaran@users.noreply.github.com>
2023-10-17 12:37:36 -0700

    Removed ensemble-vep

--- external_tools/ensembl-vep/biodbhts/lib/Bio/DB/HTS/VCF/Header.pm
+++
@@ -1,7 +0,0 @@
-package Bio::DB::HTS::VCF::Header;
-
-use Bio::DB::HTS; #load the XS
-
-$Bio::DB::HTS::VCF::Header::VERSION = '2.11';
-
-1;
--- external_tools/ensembl-vep/biodbhts/lib/Bio/DB/HTS/VCF/Row.pm
+++
@@ -1,7 +0,0 @@
-package Bio::DB::HTS::VCF::Row;
-
-$Bio::DB::HTS::VCF::Row::VERSION = '2.11';
-
-use Bio::DB::HTS;
-
-1;
