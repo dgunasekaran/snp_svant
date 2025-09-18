577e8e1e11abe19210f0fbe3c51be82b93a30ea5
dgunasekaran <47905038+dgunasekaran@users.noreply.github.com>
2025-09-17 19:33:16 -0700

    Resolves R package dependencies {#1}

--- workflow/scripts/install_r_packages.R
+++ workflow/scripts/install_r_packages.R
@@ -1,8 +1,10 @@
+BiocManager::install(version = '3.17', ask=FALSE)
+
 options(repos = c(CRAN = "https://cloud.r-project.org"))
 
 # Source: https://github.com/trevorld/r-argparse
 if (!require("argparse", quietly = TRUE)) {
-  install.packages("argparse")
+  install.packages("https://cran.r-project.org/src/contrib/Archive/argparse/argparse_2.2.2.tar.gz", repos=NULL, type="source")
 }
 
 # Source: https://ggplot2.tidyverse.org/
@@ -10,18 +12,26 @@ if (!require("ggplot2", quietly = TRUE)) {
   install.packages("ggplot2")
 }
 
-# Source: https://bioconductor.org/packages/release/bioc/html/apeglm.html
-if (!require("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
+if (!require("lattice", quietly = TRUE)) {
+  install.packages("https://cran.r-project.org/src/contrib/Archive/lattice/lattice_0.21-9.tar.gz", repos=NULL, type="source")
+}
+
+if (!require("Matrix", quietly = TRUE)) {
+  install.packages("https://cran.r-project.org/src/contrib/Archive/Matrix/Matrix_1.6-1.1.tar.gz", repos=NULL, type="source")
+}
+
+# Installing older version of BiocManager
+install.packages("https://cran.r-project.org/src/contrib/Archive/BiocManager/BiocManager_1.30.22.tar.gz", repos=NULL, type="source")
+BiocManager::install(version = '3.17', ask=FALSE)
 
 # Source: https://bioconductor.org/packages/release/bioc/html/VariantAnnotation.html
 if (!require("VariantAnnotation", quietly = TRUE)) {
-  BiocManager::install("VariantAnnotation")
+  BiocManager::install("VariantAnnotation", version="3.17")
 }
 
 # Source: https://bioconductor.org/packages/release/bioc/html/StructuralVariantAnnotation.html
 if (!require("StructuralVariantAnnotation", quietly = TRUE)) {
-  BiocManager::install("StructuralVariantAnnotation")
+  BiocManager::install("StructuralVariantAnnotation", version="3.17")
 }
 
 # Source: https://www.tidyverse.org/
